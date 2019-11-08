3c946f78ddc985321358f4d77f909eea1a56911f
Alimjan Qadir <alimjan.qadir@sentiance.com>
2019-11-08 21:07:01 +0800

    Initial commit

---
+++ src/algorithms/Dummy.kt
@@ -0,0 +1,14 @@
+package algorithms
+
+val yao = Person("Yao", 36)
+val alimjan = Person("Alimjan", 26)
+val leo = Person("Leo", 32)
+val ilyas = Person("Ilyas", 27)
+val gulzar = Person("Gulzar", 30)
+val esedullah = Person("Esedullah", 24)
+val bextiyar = Person("Bextiyar", 26)
+val jon = Person("Jon", 21)
+
+val peopleList: List<Person> = listOf(yao, alimjan, leo, ilyas, gulzar, esedullah, bextiyar, jon)
+
+val numberList = listOf(4, 5, 3, 1, 4, 10, 2, 9, 10, 3213, 3214, 1231, 111)
