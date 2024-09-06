94591cd2ae558617fc0e3f1a96f9a5e9b5a5d823
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2024-09-06 10:51:26 +0800

    Updates Merge sorted array with simpler solution.

--- src/main/kotlin/problems/MergeSortedArray.kt
+++ src/main/kotlin/problems/MergeSortedArray.kt
@@ -2,33 +2,23 @@ package problems
 
 class MergeSortedArray {
     fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int) {
-        println("m: $m")
-        println("n: $n")
-        // Pointers initialization
-        var p1 = m - 1 // Pointer for the last element in the initial part of nums1
-        var p2 = n - 1 // Pointer for the last element in nums2
-        var p = m + n - 1 // Pointer for the last position in nums1
+        // Start from the end of the filled portion of nums1
+        var i = m - 1
+        // Start from the end of nums2
+        var j = n - 1
+        // Start filling from the end of nums1
+        var k = m + n - 1
 
-        // Iterate while both arrays have elements to be compared
-        while (p1 >= 0 && p2 >= 0) {
-            // Compare elements from the end of both arrays
-            if (nums1[p1] > nums2[p2]) {
-                // If element in nums1 is larger, place it in the current position of nums1
-                nums1[p] = nums1[p1]
-                p1-- // Move pointer in nums1
+        // Merge in reverse order
+        while (j >= 0) {
+            // If nums1 has elements left and its current element is larger
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--]
             } else {
-                // If element in nums2 is larger, place it in the current position of nums1
-                nums1[p] = nums2[p2]
-                p2-- // Move pointer in nums2
+                // Either nums1 is exhausted or nums2's current element is larger
+                nums1[k--] = nums2[j--]
             }
-            p-- // Move the final position pointer
         }
-
-        // If there are still elements in nums2, copy them
-        while (p2 >= 0) {
-            nums1[p] = nums2[p2]
-            p2-- // Move pointer in nums2
-            p-- // Move the final position pointer
-        }
+        // No need to handle remaining elements in nums1, they're already in place
     }
 }
