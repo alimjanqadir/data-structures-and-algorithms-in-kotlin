f80f8564a67e60b87f5bf8c61b39bc3551e9caec
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2024-12-03 21:29:27 +0800

    update(kotlin): jvm

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "2.0.21"
+    id("org.jetbrains.kotlin.jvm") version "2.0.22"
     id("io.gitlab.arturbosch.detekt") version "1.23.0" // Use the latest version
 }
 
