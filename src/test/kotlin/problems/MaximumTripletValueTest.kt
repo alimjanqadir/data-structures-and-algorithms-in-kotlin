7e906425a4c954eb3538fad53300582ae7a1574f
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2025-04-02 13:59:12 +0800

    feat(leetcode): add solution for Maximum Value of an Ordered Triplet I

---
+++ src/test/kotlin/problems/MaximumTripletValueTest.kt
@@ -0,0 +1,13 @@
+package problems
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class TripletValueTest {
+    @Test
+    fun testMaximumTripletValue() {
+        assertEquals(77, maximumTripletValue(intArrayOf(12, 6, 1, 2, 7)))
+        assertEquals(133, maximumTripletValue(intArrayOf(1, 10, 3, 4, 19)))
+        assertEquals(0, maximumTripletValue(intArrayOf(1, 2, 3)))
+    }
+}
