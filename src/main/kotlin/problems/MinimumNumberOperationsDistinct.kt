66dfd596d4c382c36b0afe74a91d7175cea3ff5f
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2025-04-08 18:28:31 +0800

    feat(leetcode): solve Minimum Number of Operations to Make Elements in Array Distinct

---
+++ src/main/kotlin/problems/MinimumNumberOperationsDistinct.kt
@@ -0,0 +1,14 @@
+fun minimumOperations(nums: IntArray): Int {
+    val seen = mutableSetOf<Int>()
+    var suffixStart = nums.size
+
+    for (index in nums.indices.reversed()) {
+        if (!seen.add(nums[index])) {
+            break
+        }
+        suffixStart = index
+    }
+
+    val elementsToRemove = suffixStart
+    return (elementsToRemove + 2) / 3 // ceil(elementsToRemove / 3)
+}
