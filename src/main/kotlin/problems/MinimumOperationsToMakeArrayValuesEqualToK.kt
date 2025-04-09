411c1a8d1bd5ac99e5bef1dbf074bbe1df561b85
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2025-04-09 22:44:58 +0800

    feat(leetcode): solve Minimum Operations to Make Array Values Equal to K

---
+++ src/main/kotlin/problems/MinimumOperationsToMakeArrayValuesEqualToK.kt
@@ -0,0 +1,15 @@
+package problems
+
+/**
+ * Solution for Minimum Operations to Make Array Values Equal to K
+ * Time Complexity: O(n log n) due to sortedSet operations
+ * Space Complexity: O(n) for storing elements greater than k
+ */
+fun minOperations(nums: IntArray, k: Int): Int {
+    val greaterSet = sortedSetOf<Int>()
+    for (num in nums) {
+        if (num < k) return -1
+        if (num > k) greaterSet.add(num)
+    }
+    return greaterSet.size
+}
