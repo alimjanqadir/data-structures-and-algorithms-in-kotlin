ef221ab6a564b7531da6bfa2301c4c499bba80b9
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2025-04-24 01:49:06 +0800

    feat(leetcode): solve Count Complete Subarrays

---
+++ src/test/kotlin/problems/CountCompleteSubarraysTest.kt
@@ -0,0 +1,30 @@
+package problems
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CountCompleteSubarraysTest {
+    @Test
+    fun testExample1() {
+        val nums = intArrayOf(1,3,1,2,2)
+        assertEquals(4, countCompleteSubarrays(nums))
+    }
+
+    @Test
+    fun testExample2() {
+        val nums = intArrayOf(5,5,5,5)
+        assertEquals(10, countCompleteSubarrays(nums))
+    }
+
+    @Test
+    fun testAllDistinct() {
+        val nums = intArrayOf(1,2,3,4)
+        assertEquals(1, countCompleteSubarrays(nums))
+    }
+
+    @Test
+    fun testSingleElement() {
+        val nums = intArrayOf(7)
+        assertEquals(1, countCompleteSubarrays(nums))
+    }
+}
