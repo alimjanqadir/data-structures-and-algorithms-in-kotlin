b101e29724b9b88038de7ec43c37225ca75e3758
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2025-05-03 06:27:06 +0800

    feat(leetcode): implement min domino rotations solution with top-level function

---
+++ src/main/kotlin/problems/MinDominoRotations.kt
@@ -0,0 +1,32 @@
+package problems
+
+fun minDominoRotations(tops: IntArray, bottoms: IntArray): Int {
+    fun rotationsNeeded(targetValue: Int): Int {
+        var flipsToMakeTopRow   = 0
+        var flipsToMakeBottomRow = 0
+
+        for (dominoIndex in tops.indices) {
+            val topValue    = tops[dominoIndex]
+            val bottomValue = bottoms[dominoIndex]
+
+            // Candidate fails if targetValue absent in this domino
+            if (topValue != targetValue && bottomValue != targetValue) {
+                return Int.MAX_VALUE   // impossible
+            }
+
+            if (topValue != targetValue)   flipsToMakeTopRow   += 1
+            if (bottomValue != targetValue) flipsToMakeBottomRow += 1
+        }
+        return minOf(flipsToMakeTopRow, flipsToMakeBottomRow)
+    }
+
+    val firstTop    = tops[0]
+    val firstBottom = bottoms[0]
+
+    val answer = minOf(
+        rotationsNeeded(firstTop),
+        rotationsNeeded(firstBottom)
+    )
+
+    return if (answer == Int.MAX_VALUE) -1 else answer
+}
---
+++ src/test/kotlin/problems/MinDominoRotationsTest.kt
@@ -0,0 +1,34 @@
+package problems
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class MinDominoRotationsTest {
+    @Test
+    fun testExample1() {
+        val tops = intArrayOf(2, 1, 2, 4, 2, 2)
+        val bottoms = intArrayOf(5, 2, 6, 2, 3, 2)
+        assertEquals(2, minDominoRotations(tops, bottoms))
+    }
+
+    @Test
+    fun testExample2() {
+        val tops = intArrayOf(3, 5, 1, 2, 3)
+        val bottoms = intArrayOf(3, 6, 3, 3, 4)
+        assertEquals(-1, minDominoRotations(tops, bottoms))
+    }
+
+    @Test
+    fun testAllSame() {
+        val tops = intArrayOf(1, 1, 1, 1)
+        val bottoms = intArrayOf(1, 1, 1, 1)
+        assertEquals(0, minDominoRotations(tops, bottoms))
+    }
+
+    @Test
+    fun testImpossibleCase() {
+        val tops = intArrayOf(1, 2, 3, 4)
+        val bottoms = intArrayOf(5, 6, 7, 8)
+        assertEquals(-1, minDominoRotations(tops, bottoms))
+    }
+}
