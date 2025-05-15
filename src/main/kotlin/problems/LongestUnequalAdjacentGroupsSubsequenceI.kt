20ae2ef5f0065e6a40c43c5ae5bd30e6a884254e
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2025-05-15 10:44:31 +0800

    feat(leetcode): solve Longest Unequal Adjacent Groups Subsequence I

---
+++ src/main/kotlin/problems/LongestUnequalAdjacentGroupsSubsequenceI.kt
@@ -0,0 +1,23 @@
+package problems
+
+/**
+ * LeetCode 2900. Longest Unequal Adjacent Groups Subsequence I
+ * Returns the longest subsequence where adjacent words belong to different groups.
+ * @param words Array of words
+ * @param groups Array of group ids
+ * @return List<String> - the longest valid subsequence
+ */
+fun longestUnequalAdjacentGroupsSubsequenceI(words: Array<String>, groups: IntArray): List<String> {
+    val result = mutableListOf<String>()
+    if (words.isNotEmpty()) {
+        result.add(words[0])
+        var lastGroup = groups[0]
+        for (i in 1 until words.size) {
+            if (groups[i] != lastGroup) {
+                result.add(words[i])
+                lastGroup = groups[i]
+            }
+        }
+    }
+    return result
+}
