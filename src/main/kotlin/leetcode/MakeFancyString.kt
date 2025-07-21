746c3c7157143cea375bd5f9781b884749374dd6
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2025-07-21 09:14:12 +0800

    feat(leetcode): add makeFancyString

---
+++ src/main/kotlin/leetcode/MakeFancyString.kt
@@ -0,0 +1,21 @@
+package leetcode
+
+fun makeFancyString(original: String): String {
+  val fancyBuilder = StringBuilder()
+
+  for (currentCharacter in original) {
+    val lengthSoFar = fancyBuilder.length
+    val shouldDeleteCurrent =
+      lengthSoFar >= 2 &&
+        currentCharacter == fancyBuilder[lengthSoFar - 1] &&
+        currentCharacter == fancyBuilder[lengthSoFar - 2]
+
+    if (!shouldDeleteCurrent) {
+      fancyBuilder.append(currentCharacter)
+    }
+  }
+
+  return fancyBuilder.toString()
+}
+
+
