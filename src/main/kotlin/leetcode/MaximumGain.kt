d65461b1b946ae6bba48731576047b332d58dcf2
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2025-07-23 13:30:01 +0800

    feat(leetcode): implement maximum gain

---
+++ src/main/kotlin/leetcode/MaximumGain.kt
@@ -0,0 +1,56 @@
+package leetcode
+
+fun maximumGain(s: String, x: Int, y: Int): Int {
+  val firstLeft: Char
+  val firstRight: Char
+  val firstScore: Int
+  val secondLeft: Char
+  val secondRight: Char
+  val secondScore: Int
+
+  if (x >= y) {
+    firstLeft = 'a'
+    firstRight = 'b'
+    firstScore = x
+    secondLeft = 'b'
+    secondRight = 'a'
+    secondScore = y
+  } else {
+    firstLeft = 'b'
+    firstRight = 'a'
+    firstScore = y
+    secondLeft = 'a'
+    secondRight = 'b'
+    secondScore = x
+  }
+
+  val chars = s.toCharArray()
+  var total = 0
+
+  var write = 0
+  for (read in chars.indices) {
+    val c = chars[read]
+    if (write > 0 && chars[write - 1] == firstLeft && c == firstRight) {
+      write -= 1
+      total += firstScore
+    } else {
+      chars[write] = c
+      write += 1
+    }
+  }
+
+  val remaining = write
+  write = 0
+  for (read in 0 until remaining) {
+    val c = chars[read]
+    if (write > 0 && chars[write - 1] == secondLeft && c == secondRight) {
+      write -= 1
+      total += secondScore
+    } else {
+      chars[write] = c
+      write += 1
+    }
+  }
+
+  return total
+}
---
+++ src/test/kotlin/problems/MaximumGainTest.kt
@@ -0,0 +1,22 @@
+package problems
+
+import leetcode.maximumGain
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MaximumGainTest {
+  @Test
+  fun example1() {
+    assertEquals(19, maximumGain("cdbcbbaaabab", 4, 5))
+  }
+
+  @Test
+  fun example2() {
+    assertEquals(20, maximumGain("aabbaaxybbaabb", 5, 4))
+  }
+
+  @Test
+  fun single() {
+    assertEquals(1, maximumGain("abcde", 1, 1))
+  }
+}
