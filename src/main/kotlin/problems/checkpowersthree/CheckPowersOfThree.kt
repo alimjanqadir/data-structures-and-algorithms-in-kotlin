e90aae5ffebbb78387bc508d27120df527b50f25
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2025-08-14 21:16:43 +0800

    feat(problems): add checkPowersOfThree solution

---
+++ src/main/kotlin/problems/checkpowersthree/CheckPowersOfThree.kt
@@ -0,0 +1,13 @@
+package problems.checkpowersthree
+
+class Solution {
+  fun checkPowersOfThree(n: Int): Boolean {
+    var remaining = n
+    while (remaining > 0) {
+      val base3Digit = remaining % 3
+      if (base3Digit == 2) return false
+      remaining /= 3
+    }
+    return true
+  }
+}
