0d441f214e604f54bb9c2fe3f590cee1564f60ea
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2025-09-12 22:52:34 +0800

    feat(leetcode): add doesAliceWin solution

---
+++ src/main/kotlin/problems/doesalicewin/DoesAliceWin.kt
@@ -0,0 +1,14 @@
+package problems.doesalicewin
+
+class Solution {
+  fun doesAliceWin(s: String): Boolean {
+    fun isVowel(character: Char): Boolean {
+      return character == 'a' || character == 'e' || character == 'i' ||
+        character == 'o' || character == 'u'
+    }
+    for (character in s) {
+      if (isVowel(character)) return true
+    }
+    return false
+  }
+}
