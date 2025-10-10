2e9e706daa09513dda14510989b7dc3b7b8b4703
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2025-10-10 20:07:54 +0800

    fix(push): restore maxArea solution

---
+++ src/main/kotlin/problems/push/MaximumEnergy.kt
@@ -0,0 +1,27 @@
+package problems.push
+
+fun maximumEnergy(energy: IntArray, k: Int): Int {
+  val size = energy.size
+  var bestOverall = Int.MIN_VALUE
+
+  for (residue in 0 until k) {
+    var lastIndex = residue + ((size - 1 - residue) / k) * k
+
+    var suffixSum = 0
+    var bestForResidue = Int.MIN_VALUE
+
+    var index = lastIndex
+    while (index >= 0) {
+      suffixSum += energy[index]
+      if (suffixSum > bestForResidue) {
+        bestForResidue = suffixSum
+      }
+      index -= k
+    }
+
+    if (bestForResidue > bestOverall) {
+      bestOverall = bestForResidue
+    }
+  }
+  return bestOverall
+}
