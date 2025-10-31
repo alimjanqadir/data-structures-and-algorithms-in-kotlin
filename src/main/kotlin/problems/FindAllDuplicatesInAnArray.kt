ca1a775c01de2a47429e6c63a646d7e30c4f77e3
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2025-10-31 22:21:25 +0800

    feat(leetcode): solve Find All Duplicates in an Array

---
+++ src/main/kotlin/problems/FindAllDuplicatesInAnArray.kt
@@ -0,0 +1,17 @@
+package problems
+
+fun findDuplicates(nums: IntArray): IntArray {
+    val seen = mutableSetOf<Int>()
+    val result = mutableListOf<Int>()
+    
+    // Iterate over the array and track the repeated numbers
+    for (num in nums) {
+        if (num in seen) {
+            result.add(num)
+        } else {
+            seen.add(num)
+        }
+    }
+    
+    return result.toIntArray()
+}
