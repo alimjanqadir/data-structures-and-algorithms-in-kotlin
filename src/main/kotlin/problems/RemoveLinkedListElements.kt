920ce2579eddb4e782dea4f58e8a7b7798f8136c
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2025-11-01 21:19:28 +0800

    feat(leetcode): solve Remove Linked List Elements

---
+++ src/main/kotlin/problems/RemoveLinkedListElements.kt
@@ -0,0 +1,24 @@
+package problems
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+fun removeElements(head: ListNode?, `val`: Int): ListNode? {
+    val dummy = ListNode(0)
+    dummy.next = head
+    var prev: ListNode? = dummy
+    while (prev!!.next != null) {
+        if (prev.next!!.`val` == `val`) {
+            prev.next = prev.next!!.next
+        } else {
+            prev = prev.next
+        }
+    }
+    return dummy.next
+}
