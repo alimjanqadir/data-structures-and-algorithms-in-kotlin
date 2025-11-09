585f4d24e09a2f44ebbf26c743612ed303fffeee
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2025-11-10 00:22:32 +0800

    feat(leetcode): solve Count Operations to Obtain Zero

---
+++ src/main/kotlin/problems/CountOperationsToObtainZero.kt
@@ -0,0 +1,17 @@
+package problems
+
+fun countOperations(num1: Int, num2: Int): Int {
+    var a = num1
+    var b = num2
+    var count = 0
+    while (a > 0 && b > 0) {
+        if (a >= b) {
+            count += a / b
+            a %= b
+        } else {
+            count += b / a
+            b %= a
+        }
+    }
+    return count
+}
