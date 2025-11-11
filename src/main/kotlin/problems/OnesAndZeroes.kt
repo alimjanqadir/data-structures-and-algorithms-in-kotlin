364314a34bd3e1ed9a534acc12a28a713cd84974
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2025-11-11 20:07:42 +0800

    feat(leetcode): solve Ones and Zeroes

---
+++ src/main/kotlin/problems/OnesAndZeroes.kt
@@ -0,0 +1,15 @@
+package problems
+
+fun findMaxForm(strs: Array<String>, m: Int, n: Int): Int {
+    val dp = Array(m + 1) { IntArray(n + 1) }
+    for (str in strs) {
+        val zeros = str.count { it == '0' }
+        val ones = str.length - zeros
+        for (j in m downTo zeros) {
+            for (k in n downTo ones) {
+                dp[j][k] = maxOf(dp[j][k], dp[j - zeros][k - ones] + 1)
+            }
+        }
+    }
+    return dp[m][n]
+}
