e3b5800a9157425bb621ca296a3c705e3a509915
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2025-11-13 22:18:52 +0800

    feat(leetcode): solve Maximum Number of Ones

---
+++ src/main/kotlin/problems/MaximumNumberOfOnes.kt
@@ -0,0 +1,24 @@
+package problems
+
+fun maxOperations(s: String): Int {
+    val n = s.length
+    var ans = 0
+    var countOnes = 0
+    var i = 0
+    while (i < n) {
+        if (s[i] == '0') {
+            i++
+            continue
+        }
+        // start of a '1' run
+        while (i < n && s[i] == '1') {
+            countOnes++
+            i++
+        }
+        // if followed by '0's
+        if (i < n) {
+            ans += countOnes
+        }
+    }
+    return ans
+}
