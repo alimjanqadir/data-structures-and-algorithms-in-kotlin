144983b8ce73fe3f050521adcc24a334bf89daf2
Alimjan Qadir <38877166+alimjanqadir@users.noreply.github.com>
2026-02-08 17:26:04 +0800

    feat(leetcode): add Balanced Binary Tree solution

---
+++ src/main/kotlin/problems/BalancedBinaryTree.kt
@@ -0,0 +1,30 @@
+package problems
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+
+fun isBalanced(root: TreeNode?): Boolean {
+  fun checkHeight(node: TreeNode?): Int {
+    if (node == null) return 0
+    
+    val leftHeight = checkHeight(node.left)
+    if (leftHeight == -1) return -1
+    
+    val rightHeight = checkHeight(node.right)
+    if (rightHeight == -1) return -1
+    
+    if (Math.abs(leftHeight - rightHeight) > 1) return -1
+    
+    return Math.max(leftHeight, rightHeight) + 1
+  }
+  
+  return checkHeight(root) != -1
+}
